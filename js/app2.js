853c7aaa0e0a4c42b9badb2ff486ca2bd7638f47
Jeremie Miller <jeremie@jabber.org>
2015-03-17 08:51:33 -0600

    play w/ doing more decoding

--- js/app2.js
+++ js/app2.js
@@ -1,9 +1,26 @@
 (function () {
+  var base32 = telehash.base32;
+  var e3x = telehash.e3x;
+  var hashname = telehash.hashname;
+  var lob = telehash.lob;
+
   var xin = document.getElementsByName('xin')[0];
   var xout = document.getElementsByName('xout')[0];
 
   function updated () {
-    xout.value = xin.value;
+    var bin = base32.decode(xin.value);
+    if(bin.length == 32)
+    {
+      xout.value = hashname.isHashname(xin.value) ? "valid hashname":"invalid hashname";
+    }else{
+      var packet = lob.decode(bin);
+      if(packet)
+      {
+        xout.value = "packet: "+JSON.stringify(packet.json);
+      }else{
+        xout.value = "unknown input length "+bin.length;
+      }
+    }
   }
 
   xin.addEventListener('change', updated, false);
